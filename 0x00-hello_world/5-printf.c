480d6a9c9d6a792aca218c4ab5c4b7f5f2fbcbb9
SeriqueM <122870054+SeriqueM@users.noreply.github.com>
2023-02-19 20:44:31 +0000

    Advanced tasks

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/*
+*Main - Entry Point
+*
+*Return (0) = success
+*/
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,/n");
+    printf("with proper grammar, but the outcome is a piece of art,");
     return(0);
 }
